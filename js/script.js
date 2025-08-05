3d96c99d43678f0432c5cc7967dbcc5de3efed13
anivernet <ani.vernet@gmail.com>
2025-08-05 16:09:33 +0200

    замінила перемикач тем на векторне зображення, змінила колір перемикача мов

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const themeToggle = document.querySelector('.theme-toggle');
+const themeIcon = document.querySelector('.theme-icon');
+
 themeToggle.addEventListener('click', ()=> {
     document.body.classList.toggle('dark-theme');
+
+    const isDark = document.body.classList.contains('dark-theme');
+    themeIcon.src = isDark ? 'icons/theme-swicher-to-dark.svg' : 'icons-dark-theme/theme-swicher-to-light.svg';
+    themeIcon.alt = isDark ? 'Темна тема': 'Світла тема';
 });
 
 const langOptions = document.querySelectorAll('.lang-option');
